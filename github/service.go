cd998725fca4d4d34613da8fa1cc47db7e05fb02
cheenoxd <zesanrahim@gmail.com>
2026-02-04 21:07:21 -0500

    feat: add cloning service

--- github/service.go
+++ github/service.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
 	"sync"
 )
 
@@ -13,8 +16,31 @@ type Repo struct {
 	// Size        int64
 }
 
-// repo functions
-func cloneRepo(repo Repo) error  {}
-func fetchRepo(repo Repo) error  {}
-func checkRepo(repo Repo) error  {}
-func deleteRepo(repo Repo) error {}
+func cloneRepo(repo *Repo) error {
+
+	repo.Lock.Lock()
+	defer repo.Lock.Unlock()
+
+	if _, err := os.Stat(repo.RepoPath); !os.IsNotExist(err) {
+		fmt.Printf("Repo %s already exists at %s\n", repo.Name, repo.RepoPath)
+		return nil // already cloned
+	}
+
+	if err := os.MkdirAll(repo.RepoPath, os.ModePerm); err != nil {
+		return fmt.Errorf("failed to create repo directory: %w", err)
+	}
+
+	cmd := exec.Command("git", "clone", "--bare", repo.Name, repo.RepoPath)
+	output, err := cmd.Output()
+	if err != nil {
+		return fmt.Errorf("git clone failed: %w\nOutput: %s", err, string(output))
+	}
+
+	fmt.Printf("Successfully cloned repo %s\n", repo.Name)
+	return nil
+
+}
+
+func fetchRepo(repo *Repo) error  { return nil }
+func checkRepo(repo *Repo) error  { return nil }
+func deleteRepo(repo *Repo) error { return nil }
