beda121f1d37d603e84266acd5095cf6bb26f240
cheenoxd <zesanrahim@gmail.com>
2026-03-06 23:23:24 -0500

    feat: speed up search by using int rather than string

--- engine/index.go
+++ engine/index.go
@@ -8,16 +8,25 @@ import (
     "strings"
 )
 
+type u32 = uint32
+
 var (
     start = 0x02
     end   = 0x03
 )
 
 type Index struct {
-    data        []byte
+    Data        []byte
     Mmap        mmap.MMap
     LineOffsets []int   
-    Trigrams    map[string][]int
+    Trigrams    map[u32][]int
+}
+
+func bytesToTrigram(b []byte) u32 {
+    if len(b) < 3 {
+        return 0
+    }
+    return u32(b[0])<<16 | u32(b[1])<<8 | u32(b[2])
 }
 
 func (idx *Index) MapBoundaries(path string) error {
@@ -33,13 +42,13 @@ func (idx *Index) MapBoundaries(path string) error {
     }
 
     idx.Mmap = mmapData
-    idx.data = mmapData
+    idx.Data = mmapData
 
     idx.LineOffsets = []int{0}
 
     offset := 0
     for {
-        loc := bytes.IndexByte(idx.data[offset:], '\n')
+        loc := bytes.IndexByte(idx.Data[offset:], '\n')
         if loc == -1 {
             break
         }
@@ -55,14 +64,14 @@ func (idx *Index) MapBoundaries(path string) error {
 }
 
 func (idx *Index) BuildTrigrams() {
-    idx.Trigrams = make(map[string][]int)
+    idx.Trigrams = make(map[u32][]int, 1000000)
 
     numWorkers := 8
     lineChan := make(chan int, 100)
     var wg sync.WaitGroup
     var mu sync.Mutex
 
-    // Worker goroutines
+    
     for i := 0; i < numWorkers; i++ {
         wg.Add(1)
         go func() {
@@ -70,21 +79,21 @@ func (idx *Index) BuildTrigrams() {
             
             for lineNum := range lineChan {
                 startOffset := idx.LineOffsets[lineNum]
-                endOffset := len(idx.data)
+                endOffset := len(idx.Data)
                 if lineNum+1 < len(idx.LineOffsets) {
                     endOffset = idx.LineOffsets[lineNum+1]
                 }
 
-                line := bytes.TrimRight(idx.data[startOffset:endOffset], "\r\n")
+                line := bytes.TrimRight(idx.Data[startOffset:endOffset], "\r\n")
 
                 wrapped := make([]byte, 0, len(line)+2)
                 wrapped = append(wrapped, byte(start))
                 wrapped = append(wrapped, line...)
                 wrapped = append(wrapped, byte(end))
 
-                lineTrigrams := make(map[string][]int)
+                lineTrigrams := make(map[u32][]int)
                 for i := 0; i <= len(wrapped)-3; i++ {
-                    tri := string(wrapped[i : i+3])
+                    tri := bytesToTrigram(wrapped[i : i+3])
                     lineTrigrams[tri] = append(lineTrigrams[tri], lineNum)
                 }
 
@@ -115,10 +124,10 @@ func (idx *Index) GetLine(lineNum int) string {
     }
 
     lineStart := idx.LineOffsets[lineNum]
-    lineEnd := len(idx.data)
+    lineEnd := len(idx.Data)
     if lineNum+1 < len(idx.LineOffsets) {
         lineEnd = idx.LineOffsets[lineNum+1]
     }
 
-    return strings.TrimSpace(string(idx.data[lineStart:lineEnd]))
+    return strings.TrimSpace(string(idx.Data[lineStart:lineEnd]))
 }
--- engine/search.go
+++ engine/search.go
@@ -1,82 +1,148 @@
 package engine
 
-import "bytes"
+import (
+	"bytes"
+	"sync"
+	"runtime"
+)
 
-func (idx *Index) getTrigrams(query string) []string {
-    var trigrams []string
-    for i := 0; i <= len(query)-3; i++ {
-        tri := query[i : i+3]
-        trigrams = append(trigrams, tri)
-    }
-    return trigrams
+func (idx *Index) getTrigrams(query string) []u32 {
+	var trigrams []u32
+	queryBytes := []byte(query)
+	for i := 0; i <= len(queryBytes)-3; i++ {
+		tri := bytesToTrigram(queryBytes[i : i+3])
+		trigrams = append(trigrams, tri)
+	}
+	return trigrams
 }
 
 func (idx *Index) Search(query string) []int {
-    if len(query) < 3 {
-        return idx.linearSearch(query)
-    }
+	if len(query) < 3 {
+		return idx.linearSearch(query)
+	}
 
-    trigrams := idx.getTrigrams(query)
+	trigrams := idx.getTrigrams(query)
 
-    var candidateLines []int
-    for _, tri := range trigrams {
-        if lines, exists := idx.Trigrams[tri]; exists {
-            candidateLines = idx.intersect(candidateLines, lines)
-        } else {
-            return []int{}
-        }
-    }
+	var results []int
+	firstTrigram := trigrams[0]
 
-    results := idx.linearSearch(query)
+	if lines, exists := idx.Trigrams[firstTrigram]; exists {
+		results = lines
+	} else {
+		return []int{}
+	}
 
-    for _, lineNum := range results {
-        lineStart := idx.LineOffsets[lineNum]
-        lineEnd := len(idx.data)
-        if lineNum+1 < len(idx.LineOffsets) {
-            lineEnd = idx.LineOffsets[lineNum+1]
-        }
-        line := string(idx.data[lineStart:lineEnd])
-        _ = line // Use it or remove
-    }
-    return results
+	for i := 1; i < len(trigrams); i++ {
+		if lines, exists := idx.Trigrams[trigrams[i]]; exists {
+			results = idx.intersect(results, lines)
+		} else {
+			return []int{}
+		}
+	}
+
+	return idx.filterResults(results, query)
 }
 
 func (idx *Index) intersect(a, b []int) []int {
-    if len(a) == 0 {
-        return b
-    }
-    if len(b) == 0 {
-        return a
-    }
+	if len(a) == 0 {
+		return b
+	}
+	if len(b) == 0 {
+		return a
+	}
 
-    var result []int
-    i, j := 0, 0
-    for i < len(a) && j < len(b) {
-        if a[i] == b[j] {
-            result = append(result, a[i])
-            i++
-            j++
-        } else if a[i] < b[j] {
-            i++
-        } else {
-            j++
-        }
-    }
-    return result
+	var result []int
+	i, j := 0, 0
+	for i < len(a) && j < len(b) {
+		if a[i] == b[j] {
+			result = append(result, a[i])
+			i++
+			j++
+		} else if a[i] < b[j] {
+			i++
+		} else {
+			j++
+		}
+	}
+	return result
+}
+
+func (idx *Index) filterResults(lineNums []int, query string) []int {
+	var results []int
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	numWorkers := runtime.NumCPU()
+	lineChan := make(chan int, 100)
+
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for lineNum := range lineChan {
+				lineStart := idx.LineOffsets[lineNum]
+				lineEnd := len(idx.Data)
+				if lineNum+1 < len(idx.LineOffsets) {
+					lineEnd = idx.LineOffsets[lineNum+1]
+				}
+				line := idx.Data[lineStart:lineEnd]
+
+				if bytes.Contains(line, []byte(query)) {
+					mu.Lock()
+					results = append(results, lineNum)
+					mu.Unlock()
+				}
+			}
+		}()
+	}
+
+	go func() {
+		for _, lineNum := range lineNums {
+			lineChan <- lineNum
+		}
+		close(lineChan)
+	}()
+
+	wg.Wait()
+	return results
 }
 
 func (idx *Index) linearSearch(query string) []int {
-    var results []int
-    for lineNum := range idx.LineOffsets {
-        lineStart := idx.LineOffsets[lineNum]
-        lineEnd := len(idx.data)
-        if lineNum+1 < len(idx.LineOffsets) {
-            lineEnd = idx.LineOffsets[lineNum+1]
-        }
-        line := idx.data[lineStart:lineEnd]
-        if bytes.Contains(line, []byte(query)) {
-            results = append(results, lineNum)
-        }
-    }
-    return results
+	var results []int
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	numWorkers := runtime.NumCPU()
+	lineChan := make(chan int, 100)
+
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for lineNum := range lineChan {
+				lineStart := idx.LineOffsets[lineNum]
+				lineEnd := len(idx.Data)
+				if lineNum+1 < len(idx.LineOffsets) {
+					lineEnd = idx.LineOffsets[lineNum+1]
+				}
+				line := idx.Data[lineStart:lineEnd]
+
+				if bytes.Contains(line, []byte(query)) {
+					mu.Lock()
+					results = append(results, lineNum)
+					mu.Unlock()
+				}
+			}
+		}()
+	}
+
+	go func() {
+		for lineNum := range idx.LineOffsets {
+			lineChan <- lineNum
+		}
+		close(lineChan)
+	}()
+
+	wg.Wait()
+	return results
 }
